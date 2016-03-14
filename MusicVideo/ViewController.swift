3f27c5daf606f26679502610efb5738dc10daf37
Justin Lee <justinlee1@gmail.com>
2016-03-14 10:34:18 -0500

    Part 8 completed

--- MusicVideo/ViewController.swift
+++ MusicVideo/ViewController.swift
@@ -23,6 +23,9 @@ class ViewController: UIViewController {
     }
     
     func didLoadData(videos: [Videos]) {
+        
+        print(reachabilityStatus)
+        
         self.videos = videos
         
         for item in videos {

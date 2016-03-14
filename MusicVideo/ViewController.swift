96c6d58f170a7e99859566fe7d2fac5f7a34c85c
Justin Lee <justinlee1@gmail.com>
2016-03-14 10:55:37 -0500

    Part 9 completed

--- MusicVideo/ViewController.swift
+++ MusicVideo/ViewController.swift
@@ -12,10 +12,16 @@ class ViewController: UIViewController {
 
     var videos = [Videos]()
     
+    @IBOutlet weak var displayLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
   
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "reachabilityStatusChanged", name: "ReachStatusChanged", object: nil)
+        
+        reachabilityStatusChanged()
+        
         // Call API
         let api = APIManager()
         api.loadData("https://itunes.apple.com/us/rss/topmusicvideos/limit=10/json", completion: didLoadData)
@@ -35,6 +41,22 @@ class ViewController: UIViewController {
 //        for (index, item) in videos.enumerate() {
 //            print("\(index) name = \(item.vName)")
 //        }
+    }
+    
+    func reachabilityStatusChanged() {
+        switch reachabilityStatus {
+        case NOACCESS : view.backgroundColor = UIColor.redColor()
+            displayLabel.text = "No Internet"
+        case WIFI : view.backgroundColor = UIColor.greenColor()
+            displayLabel.text = "Reachable with WIFI"
+        case WWAN : view.backgroundColor = UIColor.yellowColor()
+            displayLabel.text = "Reachable with Cellular"
+        default : return
+        }
+    }
+    
+    deinit {
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: "ReachStatusChanged", object: nil)
     }
 }
 

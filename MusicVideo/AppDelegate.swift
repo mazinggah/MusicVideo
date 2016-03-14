c564664df8cafbe6ceb32949b17756e989cdbdb3
Justin Lee <justinlee1@gmail.com>
2016-03-14 11:12:16 -0500

    Part 10 completed

--- MusicVideo/AppDelegate.swift
+++ MusicVideo/AppDelegate.swift
@@ -27,6 +27,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         internetCheck = Reachability.reachabilityForInternetConnection()
         internetCheck?.startNotifier()
+        statusChangedWithReachability(internetCheck!)
         
         return true
     }
